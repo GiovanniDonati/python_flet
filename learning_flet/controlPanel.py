8c8598962d1444842e1457e3de8e002352406f40
Giovanni Donati <grdonati99@gmail.com>
2024-07-31 05:13:26 -0300

    função cadastrar

--- learning_flet/controlPanel.py
+++ learning_flet/controlPanel.py
@@ -3,21 +3,27 @@ import flet as ft
 def main(page: ft.Page ):
     page.title = "Capacidade e Demanda"
 
-    def pesquisar(e):
-        pass
+    lista_produtos = ft.ListView()
+    
+    def adicionar(e):
+        lista_produtos.controls.append(ft.Text(f"Código: {codigo.value}, Quantidade: {quantidade.value}"))
+        page.update()
 
     txt_codigo = ft.Text('Código do item:')
     codigo = ft.TextField(label='Digite o código', text_align=ft.TextAlign.LEFT)
     txt_quantidade = ft.Text('Quantidade do item:')
     quantidade = ft.TextField(label='Digite a quantidade', text_align=ft.TextAlign.LEFT)
-    btn_produto = ft.ElevatedButton('Calcular', on_click=pesquisar)
+    btn_produto = ft.ElevatedButton('Calcular', on_click=adicionar)
+
 
     page.add(
         txt_codigo,
         codigo,
         txt_quantidade,
         quantidade,
-        btn_produto
+        btn_produto,
+        lista_produtos,
     )
+
 
 ft.app(target=main)
